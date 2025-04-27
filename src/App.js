abcac6b5c89992a08a94e76e3b954137574c18bc
Sonali <sonalibiswas242@gmail.com>
2025-04-27 08:50:56 -0700

    updated png commit

--- src/App.js
+++ src/App.js
@@ -179,7 +179,7 @@ const App = () => {
           width: 100%;
           position: relative;
           background: none;
-          margin-top: 0px; /* mountain will come after the content */
+          margin-top: 20px; /* mountain will come after the content */
         }
 
         .mountain-layer {
